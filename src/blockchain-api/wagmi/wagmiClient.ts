538a5d5b02b7616e1219e9eeb30abaadaeb46dee
m66260 <m66260@protonmail.com>
2023-07-24 19:08:16 -0400

    no mainnet yet, zk first

--- src/blockchain-api/wagmi/wagmiClient.ts
+++ src/blockchain-api/wagmi/wagmiClient.ts
@@ -1,20 +1,18 @@
 import { Chain, connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { coinbaseWallet, injectedWallet, metaMaskWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets';
 import { configureChains, createClient } from 'wagmi';
-import { polygonMumbai, polygon, polygonZkEvm, polygonZkEvmTestnet } from 'wagmi/chains';
+import { polygonMumbai, polygonZkEvm, polygonZkEvmTestnet } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
-import polygonMainIcon from 'assets/networks/polygonMain.svg';
 import polygonTestIcon from 'assets/networks/polygonTest.svg';
 import zkMainIcon from 'assets/networks/zkEvmMain.svg';
 import zkTestIcon from 'assets/networks/zkEvmTest.svg';
 import { config } from 'config';
 
 const defaultChains: Chain[] = [
+  // { ...polygonZkEvm, iconUrl: zkMainIcon, iconBackground: 'transparent' }, // not yet available (to be added later)
   { ...polygonMumbai, iconUrl: polygonTestIcon, iconBackground: 'transparent' },
-  { ...polygon, iconUrl: polygonMainIcon, iconBackground: 'transparent' },
-  { ...polygonZkEvm, iconUrl: zkMainIcon, iconBackground: 'transparent' },
   { ...polygonZkEvmTestnet, iconUrl: zkTestIcon, iconBackground: 'transparent' },
 ];
 
