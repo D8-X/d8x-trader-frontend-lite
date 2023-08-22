c797d337d0d8e742527fdb785dc543d312d80c67
c40003141 <c40003141@protonmail.com>
2023-08-22 15:35:14 +0200

    ensure position and order data renders as table on small laptops

--- src/components/open-orders-table/elements/OpenOrderRow.tsx
+++ src/components/open-orders-table/elements/OpenOrderRow.tsx
@@ -1,6 +1,8 @@
 import { format } from 'date-fns';
 import { useTranslation } from 'react-i18next';
 
+import IconButton from '@mui/material/IconButton';
+import { DeleteForeverOutlined } from '@mui/icons-material';
 import { Button, TableCell, TableRow, Typography } from '@mui/material';
 
 import { parseSymbol } from 'helpers/parseSymbol';
@@ -8,6 +10,7 @@ import type { OrderWithIdI } from 'types/types';
 import { formatToCurrency } from 'utils/formatToCurrency';
 
 import { typeToLabelMap } from '../typeToLabelMap';
+import styles from './OpenOrderRow.module.scss';
 
 interface OpenOrderRowPropsI {
   order: OrderWithIdI;
@@ -64,9 +67,13 @@ export const OpenOrderRow = ({ order, handleOrderCancel }: OpenOrderRowPropsI) =
         <Typography variant="cellSmall">{deadlineDate}</Typography>
       </TableCell>
       <TableCell align="center">
-        <Button variant="primary" size="tableSmall" onClick={() => handleOrderCancel(order)}>
-          {t('pages.trade.orders-table.table-content.cancel')}
-        </Button>
+        <IconButton
+          aria-label={t('pages.trade.orders-table.table-content.cancel')}
+          title={t('pages.trade.positions-table.modify-modal.cancel')}
+          onClick={() => handleOrderCancel(order)}
+        >
+          <DeleteForeverOutlined className={styles.actionIcon} />
+        </IconButton>
       </TableCell>
     </TableRow>
   );
