b07008bb9f777b1d9d7f99d7db393b67a98e588d
c40003141 <c40003141@protonmail.com>
2023-09-19 23:02:28 +0200

    improve TV fix while keeping check to discard past updates

--- src/components/trading-view-chart/TradingViewChart.tsx
+++ src/components/trading-view-chart/TradingViewChart.tsx
@@ -26,7 +26,12 @@ export const TradingViewChart = memo(() => {
     if (newCandle == null || !seriesRef.current || !latestCandleTimeRef.current) {
       return;
     }
-    seriesRef.current.update(newCandle);
+
+    const latestCandleTime = latestCandleTimeRef.current || 0;
+    if (newCandle.time >= latestCandleTime) {
+      seriesRef.current.update(newCandle);
+      latestCandleTimeRef.current = newCandle.time;
+    }
 
     setNewCandle(null);
   }, [newCandle, setNewCandle]);
