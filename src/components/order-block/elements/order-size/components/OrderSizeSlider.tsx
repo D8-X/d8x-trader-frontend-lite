b9261aa5603d43647e4c786010b4afb62301ddec
SpasiboKojima <andrei.katsewich@yandex.ru>
2023-09-20 17:22:52 +0300

    Add roundToLotString to Slider's set order size

--- src/components/order-block/elements/order-size/components/OrderSizeSlider.tsx
+++ src/components/order-block/elements/order-size/components/OrderSizeSlider.tsx
@@ -1,65 +1,14 @@
-import { atom, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
 
 import { Slider } from '@mui/material';
 
-import { leverageAtom, orderBlockAtom, orderSizeAtom } from 'store/order-block.store';
-import { poolTokenBalanceAtom, positionsAtom, selectedPoolAtom } from 'store/pools.store';
-import { OrderBlockE } from 'types/enums';
-
 import styles from '../OrderSize.module.scss';
-import { selectedCurrencyAtom } from '../store';
+import { setSizeFromSliderAtom } from '../store';
 
 const multipliers = [0, 0.25, 0.5, 0.75, 1];
 const marks = multipliers.map((multiplier) => ({ value: multiplier * 100, label: `${multiplier * 100}%` }));
 
-const valueLabelFormat = (value: number) => `${value}%`;
-
-export const maxOrderSizeAtom = atom((get) => {
-  const selectedPool = get(selectedPoolAtom);
-  const leverage = get(leverageAtom);
-  const poolTokenBalance = get(poolTokenBalanceAtom);
-  const orderBlock = get(orderBlockAtom);
-  const selectedCurrency = get(selectedCurrencyAtom);
-  if (!poolTokenBalance || !selectedPool) return;
-
-  const buffer = (1.0005 + leverage * 0.008) * 1.01;
-  const selectedPerpetual = selectedPool.perpetuals.find((perpetual) => perpetual.baseCurrency === selectedCurrency);
-  if (!selectedPerpetual) return;
-  const { collToQuoteIndexPrice, indexPrice } = selectedPerpetual;
-  let collateralCC = 0;
-  if (orderBlock !== OrderBlockE.Long) {
-    const positions = get(positionsAtom);
-    const selectedPerpetualSymbol = `${selectedPerpetual.baseCurrency}-${selectedPerpetual.quoteCurrency}-${selectedPool.poolSymbol}`;
-    const openPosition = positions.find((position) => position.symbol === selectedPerpetualSymbol);
-    collateralCC = openPosition?.collateralCC || 0;
-  }
-
-  const max = ((poolTokenBalance + collateralCC) * leverage * collToQuoteIndexPrice) / (indexPrice * buffer);
-  return max;
-});
-
-export const setSizeFromSliderAtom = atom(
-  (get) => {
-    const max = get(maxOrderSizeAtom);
-    if (!max) return 0;
-    const orderSize = get(orderSizeAtom);
-
-    const percent = (orderSize * 100) / max;
-    console.log('percent :>> ', { percent, orderSize, max });
-
-    return percent;
-  },
-  async (get, set, percent: number) => {
-    const max = get(maxOrderSizeAtom);
-    if (!max) return;
-    const orderSize = (max * percent) / 100;
-    console.log('orderSize :>> ', { percent, orderSize, max });
-
-    set(orderSizeAtom, orderSize);
-
-    return orderSize;
-  }
-);
+const valueLabelFormat = (value: number) => `${Math.floor(value)}%`;
 
 export const OrderSizeSlider = () => {
   const [sliderPercent, setSizeFromSlider] = useAtom(setSizeFromSliderAtom);
--- src/components/order-block/elements/order-size/store.ts
+++ src/components/order-block/elements/order-size/store.ts
@@ -1,3 +1,59 @@
+import { roundToLotString } from '@d8x/perpetuals-sdk';
 import { atom } from 'jotai';
 
+import { leverageAtom, orderBlockAtom, orderSizeAtom } from 'store/order-block.store';
+import {
+  perpetualStaticInfoAtom,
+  poolTokenBalanceAtom,
+  positionsAtom,
+  selectedPerpetualAtom,
+  selectedPoolAtom,
+} from 'store/pools.store';
+import { OrderBlockE } from 'types/enums';
+
 export const selectedCurrencyAtom = atom('');
+
+export const maxOrderSizeAtom = atom((get) => {
+  const selectedPool = get(selectedPoolAtom);
+  const poolTokenBalance = get(poolTokenBalanceAtom);
+  const selectedPerpetual = get(selectedPerpetualAtom);
+  if (!poolTokenBalance || !selectedPool || !selectedPerpetual) return;
+
+  const leverage = get(leverageAtom);
+  const orderBlock = get(orderBlockAtom);
+
+  const buffer = (1.0005 + leverage * 0.008) * 1.01;
+  const { collToQuoteIndexPrice, indexPrice } = selectedPerpetual;
+  let collateralCC = 0;
+  if (orderBlock !== OrderBlockE.Long) {
+    const positions = get(positionsAtom);
+    const selectedPerpetualSymbol = `${selectedPerpetual.baseCurrency}-${selectedPerpetual.quoteCurrency}-${selectedPool.poolSymbol}`;
+    const openPosition = positions.find((position) => position.symbol === selectedPerpetualSymbol);
+    collateralCC = openPosition?.collateralCC || 0;
+  }
+
+  return ((poolTokenBalance + collateralCC) * leverage * collToQuoteIndexPrice) / (indexPrice * buffer);
+});
+
+export const setSizeFromSliderAtom = atom(
+  (get) => {
+    const max = get(maxOrderSizeAtom);
+    if (!max) return 0;
+    const orderSize = get(orderSizeAtom);
+
+    return (orderSize * 100) / max;
+  },
+  async (get, set, percent: number) => {
+    const max = get(maxOrderSizeAtom);
+    const perpetualStaticInfo = get(perpetualStaticInfoAtom);
+    if (!max || !perpetualStaticInfo) return;
+
+    const orderSize = (max * percent) / 100;
+
+    const roundedValueBase = Number(roundToLotString(orderSize, perpetualStaticInfo.lotSizeBC));
+
+    set(orderSizeAtom, roundedValueBase);
+
+    return orderSize;
+  }
+);
