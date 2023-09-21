0ceeffe04245da5f34c4308e89d49f1b47e11cba
m66260 <m66260@protonmail.com>
2023-09-21 12:43:36 -0400

    remove log

--- src/helpers/getDelegateKey.ts
+++ src/helpers/getDelegateKey.ts
@@ -3,15 +3,12 @@ import CryptoJS from 'crypto-js';
 import { WalletClient } from 'viem';
 
 export function getDelegateKey(walletClient: WalletClient, storageKey: string) {
-  console.log('getDelegateKey');
   const encoded = secureLocalStorage.getItem(`delegateKey-${walletClient.account}`);
   if (encoded === null) {
-    console.log('null key');
     return undefined;
   } else {
     const bytes = CryptoJS.AES.decrypt(encoded as string, storageKey);
     try {
-      console.log(bytes);
       return bytes.toString(CryptoJS.enc.Utf8);
     } catch (err) {
       throw new Error('Invalid storaget key');
