8eb519e6c34bb85afba1d68f95c30246f4aa8f2d
m66260 <117136064+m66260@users.noreply.github.com>
2024-03-05 15:29:49 -0500

    Update src/network/broker.ts
    Co-authored-by: VictorOvchinnikovD8X <157629120+VictorOvchinnikovD8X@users.noreply.github.com>

--- src/network/broker.ts
+++ src/network/broker.ts
@@ -14,7 +14,7 @@ export function orderSubmitted(chainId: number, orderIds: string[]) {
   const requestOptions = {
     ...getRequestOptions(RequestMethodE.Post),
     body: JSON.stringify({
-      orderIds: orderIds,
+      orderIds,
     }),
   };
   return fetch(`${baseUrl}/orders-submitted`, requestOptions).then((data) => {
