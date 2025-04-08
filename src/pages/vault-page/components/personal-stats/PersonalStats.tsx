22df8fe9cfdd1bedb542cbf0b3bd4facd0102ea8
c40003141 <117669273+c40003141@users.noreply.github.com>
2025-04-08 14:26:33 +0200

    remove old withdrawal stats

--- src/pages/vault-page/components/personal-stats/PersonalStats.tsx
+++ src/pages/vault-page/components/personal-stats/PersonalStats.tsx
@@ -1,22 +1,13 @@
-import { useAtom, useAtomValue } from 'jotai';
-import { memo, useEffect, useRef, useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useAccount } from 'wagmi';
 
 import { Box, Typography } from '@mui/material';
 
 import { InfoLabelBlock } from 'components/info-label-block/InfoLabelBlock';
-import { getEarnings } from 'network/history';
-import { collateralToSettleConversionAtom, flatTokenAtom, selectedPoolAtom, traderAPIAtom } from 'store/pools.store';
-import {
-  sdkConnectedAtom,
-  triggerUserStatsUpdateAtom,
-  userAmountAtom,
-  withdrawalOnChainAtom,
-  withdrawalsAtom,
-} from 'store/vault-pools.store';
-import { isEnabledChain } from 'utils/isEnabledChain';
-import { formatToCurrency, valueToFractionDigits } from 'utils/formatToCurrency';
+import { flatTokenAtom, selectedPoolAtom } from 'store/pools.store';
+import { withdrawalOnChainAtom, withdrawalsAtom } from 'store/vault-pools.store';
+import { formatToCurrency } from 'utils/formatToCurrency';
 
 import styles from './PersonalStats.module.scss';
 
@@ -26,113 +17,21 @@ interface PersonalStatsPropsI {
 
 export const PersonalStats = memo(({ withdrawOn }: PersonalStatsPropsI) => {
   const { t } = useTranslation();
-
-  const { address, chainId } = useAccount();
+  const flatToken = useAtomValue(flatTokenAtom);
 
   const selectedPool = useAtomValue(selectedPoolAtom);
   const withdrawals = useAtomValue(withdrawalsAtom);
-  const traderAPI = useAtomValue(traderAPIAtom);
-  const triggerUserStatsUpdate = useAtomValue(triggerUserStatsUpdateAtom);
-  const isSDKConnected = useAtomValue(sdkConnectedAtom);
   const hasOpenRequestOnChain = useAtomValue(withdrawalOnChainAtom);
-  const c2s = useAtomValue(collateralToSettleConversionAtom);
-  const flatToken = useAtomValue(flatTokenAtom);
-  const [userAmount, setUserAmount] = useAtom(userAmountAtom);
-
-  const [estimatedEarnings, setEstimatedEarnings] = useState<number | null>(null);
-
-  const earningsRequestSentRef = useRef(false);
-
-  const [userPrice, userSymbol] =
-    !!flatToken && selectedPool?.poolId === flatToken.poolId && !!flatToken.registeredSymbol
-      ? [flatToken.compositePrice ?? 1, flatToken.registeredSymbol]
-      : [1, selectedPool?.poolSymbol ?? ''];
-
-  useEffect(() => {
-    setUserAmount(null);
-    if (selectedPool?.poolSymbol && traderAPI && isSDKConnected && address && isEnabledChain(chainId)) {
-      traderAPI.getPoolShareTokenBalance(address, selectedPool.poolSymbol).then((amount) => {
-        setUserAmount(amount);
-      });
-    }
-  }, [selectedPool?.poolSymbol, traderAPI, isSDKConnected, address, chainId, triggerUserStatsUpdate, setUserAmount]);
-
-  useEffect(() => {
-    if (!selectedPool?.poolSymbol || !address || !isEnabledChain(chainId)) {
-      setEstimatedEarnings(null);
-      return;
-    }
-
-    if (earningsRequestSentRef.current) {
-      return;
-    }
-
-    earningsRequestSentRef.current = true;
-
-    getEarnings(chainId, address, selectedPool.poolSymbol)
-      .then(({ earnings }) => setEstimatedEarnings(earnings < -0.0000000001 ? earnings : Math.max(earnings, 0)))
-      .catch((error) => {
-        console.error(error);
-        setEstimatedEarnings(null);
-      })
-      .finally(() => {
-        earningsRequestSentRef.current = false;
-      });
-
-    return () => {
-      earningsRequestSentRef.current = false;
-    };
-  }, [chainId, address, selectedPool?.poolSymbol, triggerUserStatsUpdate]);
 
   const shareSymbol = `d${selectedPool?.settleSymbol}`;
 
+  const [userSymbol] =
+    !!flatToken && selectedPool?.poolId === flatToken.poolId && !!flatToken.registeredSymbol
+      ? [flatToken.registeredSymbol]
+      : [selectedPool?.poolSymbol ?? ''];
+
   return (
     <Box className={styles.root}>
-      <Box className={styles.leftColumn}>
-        <Box key="amount" className={styles.statContainer}>
-          <Box className={styles.statLabel}>
-            <InfoLabelBlock
-              title={t('pages.vault.personal-stats.amount.title')}
-              content={<Typography>{t('pages.vault.personal-stats.amount.info', { shareSymbol })}</Typography>}
-            />
-          </Box>
-          <Typography variant="bodyMedium" className={styles.statValue}>
-            {userAmount !== null
-              ? formatToCurrency(userAmount, shareSymbol, true, Math.min(valueToFractionDigits(userAmount), 5))
-              : '--'}
-          </Typography>
-        </Box>
-        <Box key="estimatedEarnings" className={styles.statContainer}>
-          <Box className={styles.statLabel}>
-            <InfoLabelBlock
-              title={t('pages.vault.personal-stats.earnings.title')}
-              content={
-                <>
-                  <Typography>{t('pages.vault.personal-stats.earnings.info1')}</Typography>
-                  <Typography>
-                    {t('pages.vault.personal-stats.earnings.info2', { poolSymbol: userSymbol, shareSymbol })}
-                  </Typography>
-                </>
-              }
-            />
-          </Box>
-          <Typography variant="bodyMedium" className={styles.statValue}>
-            {estimatedEarnings !== null && selectedPool
-              ? formatToCurrency(
-                  estimatedEarnings * (c2s.get(selectedPool.poolSymbol)?.value ?? 1) * userPrice,
-                  userSymbol,
-                  true,
-                  Math.min(
-                    valueToFractionDigits(
-                      estimatedEarnings * (c2s.get(selectedPool.poolSymbol)?.value ?? 1) * userPrice
-                    ),
-                    5
-                  )
-                )
-              : '--'}
-          </Typography>
-        </Box>
-      </Box>
       <Box className={styles.rightColumn}>
         <Box key="withdrawalInitiated" className={styles.statContainer}>
           <Box className={styles.statLabel}>
